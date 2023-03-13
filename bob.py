962cd0bb4858f66ced84e645f46393086ae35677
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-12 20:31:33 -0500

    Update bob.py

--- bob.py
+++ bob.py
@@ -6,8 +6,8 @@ import random
 """
 chestPriority = {"Chest Primary": ["Barbell bench press 5x12", "incline dumbbell press 5x12", "Barbell incline dumbell press 5x12", "Dumbbell bench press 5x12", "Decline Dumbell press 5x12", "Decline barbell press 5x12"]}
 chestSecondary = {"Chest Secondary": ["Decline flys 4x12","Incline flys 4x12","Flys 4x12","Dips 6x12","Band push-ups 4x12","Straight bar dips 6x12"]}
-legPriority = {"Leg Primary": ["Leg Priority 1","Leg Priority 2","Leg Priority 3","Leg Priority 4"]}
-legSecondary = {"Leg Secondary":["Leg Secondary 1","Leg Secondary 2","Leg Secondary 3","Leg Secondary 4","Leg Secondary 5","Leg Secondary 6","Leg Secondary 7","Leg Secondary 8","Leg Secondary 9","Leg Secondary 10","Leg Secondary 11","Leg Secondary 12","Leg Secondary 13","Leg Secondary 14","Leg Secondary 15","Leg Secondary 16","Leg Secondary 17","Leg Secondary 18","Leg Secondary 19"]}
+legPriority = {"Leg Primary": ["Back Squat 5x12","","Front Squat 5x12"]}
+legSecondary = {"Leg Secondary":["Lunges 5x12","Leg extensions 4x12","Calf raises 4x12","Reverse lunge 4x12","Split squats 4x12","Step-up 4x12"]}
 backPriority = {"Back Primary":["Back Priority 1","Back Priority 2","Back Priority 3","Back Priority 4","Back Priority 5",]}
 backSecondary = {"Back Secondary":["Back Secondary 1","Back Secondary 2","Back Secondary 3","Back Secondary 4","Back Secondary 5","Back Secondary 6","Back Secondary 7","Back Secondary 8","Back Secondary 9","Back Secondary 10","Back Secondary 11","Back Secondary 12","Back Secondary 13","Back Secondary 14","Back Secondary 15","Back Secondary 16","Back Secondary 17","Back Secondary 18","Back Secondary 19","Back Secondary 20"]}
 tricep ={"Tricep Workout":["Skullcrushers 4x12", "Rope pull-downs 4x12", "tricep kick-backs 4x12", "band diamond push-ups 4x12"]}
