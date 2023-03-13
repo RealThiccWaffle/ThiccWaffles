eba7a821744f39a1e78f30e4ba6d7c3a2e517452
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-12 19:35:31 -0500

    Update bob.py

--- bob.py
+++ bob.py
@@ -20,11 +20,10 @@ todaysExcersize = st.radio("What we doing today? Chest, leg, back? ", ('chest', 
 todaysSecondaryExcersize = st.radio("Do you want to add these to your workout", ('tricep', 'bicep', 'core', 'none'))
 priorityNumber = st.slider("Number of priority excersizes: ", 0, 4)
 secondaryNumber = st.slider("Number of secondary exersizes: ", 0, 4)
-bf = int(priorityNumber)
 def randomizer(excersize, priority, secondary):
     if excersize == "chest":
        x = pd.DataFrame(chestPriority)
-       y = x.sample(n = bf)
+       y = x.sample(n = priority)
        st.write(y)
        a = pd.DataFrame(chestSecondary)
        b = a.sample(n = int(secondary))
@@ -66,5 +65,5 @@ def randomizer(excersize, priority, secondary):
       #st.write(*random.sample(bicep, 4), sep = "\n"
     else:
        st.write("waffles")
-st.write(randomizer(todaysExcersize, bf, secondaryNumber))
-st.write(randomizer(todaysSecondaryExcersize, 4, 4))
+st.write(randomizer(todaysExcersize, priorityNumber, secondaryNumber))
+st.write(randomizer(todaysSecondaryExcersize, 0, 0))
