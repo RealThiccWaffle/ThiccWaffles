56c27f2a3251ab385d9432831369c44ace167cb7
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-18 21:38:05 -0500

    Update bob.py

--- bob.py
+++ bob.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 import random
@@ -38,6 +37,7 @@ user_db = pd.DataFrame(columns=['Username', 'Max Lift', 'Max Reps'])
 
 # Define the streamlit app
 def app():
+    st.set_page_config(page_title='Workout Generator', page_icon=':muscle:', layout='wide')
     st.title('Random Workout Generator')
 
     # Check if the user is logged in
@@ -54,24 +54,30 @@ def app():
     st.write('Max Reps: ' + str(user_data['Max Reps'].values[0]))
 
     # Allow the user to select their training type and muscle group
-    training_type = st.selectbox('Select your training type:', workout_types)
-    muscle_group = st.selectbox('Select your muscle group:', muscle_groups)
+    st.write('Select your training type and muscle group:')
+    col1, col2 = st.columns([2, 1])
+    with col1:
+        training_type = st.selectbox('Training Type', workout_types)
+    with col2:
+        muscle_group = st.selectbox('Muscle Group', muscle_groups)
 
     # Generate a random workout based on the selected training type and muscle group
-    exercises = training_models[muscle_group][
-
     exercises = training_models[muscle_group][training_type]
-workout = random.sample(exercises, 3) # Select 3 random exercises
+    workout = random.sample(exercises, 3) # Select 3 random exercises
 
-# Display the workout to the user
-st.write('Your workout for today:')
-for i, exercise in enumerate(workout):
-    st.write(str(i+1) + '. ' + exercise)
+    # Display the workout to the user
+    st.write('Your workout for today:')
+    for i, exercise in enumerate(workout):
+        st.write(str(i+1) + '. ' + exercise)
 
-# Allow the user to enter their main lift max and reps
-st.write('Enter your main lift max and reps:')
-max_lift = st.number_input('Max Lift', value=user_data['Max Lift'].values[0])
-max_reps = st.number_input('Max Reps', value=user_data['Max Reps'].values[0])
+    # Allow the user to enter their main lift max and reps
+    st.write
+'Enter your main lift max and reps:')
+col1, col2 = st.columns(2)
+with col1:
+    max_lift = st.number_input('Max Lift', value=user_data['Max Lift'].values[0], step=5)
+with col2:
+    max_reps = st.number_input('Max Reps', value=user_data['Max Reps'].values[0], step=5)
 
 # Update the user's data
 user_db.loc[user_data.index[0], 'Max Lift'] = max_lift
@@ -80,4 +86,10 @@ user_db.loc[user_data.index[0], 'Max Reps'] = max_reps
 # Display the user's data in a dataframe and graph
 st.write('Your data:')
 st.write(user_db)
-st.line_chart(user_db[['Max Lift', 'Max Reps']])
+
+# Show a graph of the user's progress over time
+user_history = user_db[user_db['Username'] == st.session_state['username']]
+user_history = user_history[['Max Lift', 'Max Reps']]
+user_history['Date'] = pd.to_datetime(user_db.index)
+user_history = user_history.set_index('Date')
+st.line_chart(user_history, use_container_width=True)
